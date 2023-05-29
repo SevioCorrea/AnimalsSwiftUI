33d6c74ec140943eac6f9b8a0d7aa0627e9c01b6
SevioCorrea <102820124+SevioCorrea@users.noreply.github.com>
2023-05-29 12:25:22 -0300

    First Commit

---
+++ AnimalsSwiftUI/AnimalsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalsSwiftUIApp.swift
+//  AnimalsSwiftUI
+//
+//  Created by Sévio Basilio Corrêa on 29/05/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimalsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimalsSwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AnimalsSwiftUI
+//
+//  Created by Sévio Basilio Corrêa on 29/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
