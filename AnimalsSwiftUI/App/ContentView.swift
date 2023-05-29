51efc30b0e6fa612a2803d78cbdc8d280155b0af
SevioCorrea <102820124+SevioCorrea@users.noreply.github.com>
2023-05-29 15:54:13 -0300

    Cover Images with Parsing JSON Datas

--- AnimalsSwiftUI/App/ContentView.swift
+++ AnimalsSwiftUI/App/ContentView.swift
@@ -8,11 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // MARK: - Propriedades
+    
+    // MARK: - Body
     var body: some View {
-        Text("Content")
-        .padding()
+        NavigationView {
+            List {
+                CoverImageView()
+                    .frame(height: 300)
+                    .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+            } //: List
+            .listStyle(PlainListStyle())
+            .navigationBarTitle("Africa", displayMode: .large)
+        } //: Navigation
     }
 }
+
+// MARK: - Preview
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ AnimalsSwiftUI/Model/CoverImageModel.swift
@@ -0,0 +1,15 @@
+//
+//  CoverImageModel.swift
+//  AnimalsSwiftUI
+//
+//  Created by Sévio Basilio Corrêa on 29/05/23.
+//
+
+import SwiftUI
+
+
+
+struct CoverImage: Codable, Identifiable {
+    let id: Int
+    let name: String
+}
