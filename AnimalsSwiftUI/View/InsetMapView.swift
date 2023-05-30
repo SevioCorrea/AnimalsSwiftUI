cf8f6d4d939afdc7ae1258c67382e8b78759aab5
SevioCorrea <102820124+SevioCorrea@users.noreply.github.com>
2023-05-30 14:19:55 -0300

    MapKit DetailView

---
+++ AnimalsSwiftUI/View/InsetMapView.swift
@@ -0,0 +1,54 @@
+//
+//  InsetMapView.swift
+//  AnimalsSwiftUI
+//
+//  Created by Sévio Basilio Corrêa on 30/05/23.
+//
+
+import SwiftUI
+import MapKit
+
+
+struct InsetMapView: View {
+    
+    // MARK: - Propriedades
+    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 6.600286, longitude: 16.4377599), span: MKCoordinateSpan(latitudeDelta: 60.0, longitudeDelta: 60.0))
+    
+    // MARK: - Body
+    var body: some View {
+        Map(coordinateRegion: $region)
+            .overlay(
+                NavigationLink(destination: MapView()) {
+                    HStack {
+                        Image(systemName: "mappin.circle")
+                            .foregroundColor(Color.white)
+                            .imageScale(.large)
+                        
+                        Text("Locations")
+                            .foregroundColor(.accentColor)
+                            .fontWeight(.bold)
+                    } //: HStack
+                    .padding(.vertical, 10)
+                    .padding(.horizontal, 14)
+                    .background(
+                        Color.black
+                            .opacity(0.4)
+                            .cornerRadius(8)
+                    )
+                } //: Navigation
+                    .padding(12), alignment: .topTrailing
+            )
+            .frame(height: 256)
+            .cornerRadius(12)
+    }
+}
+
+// MARK: - Preview
+
+struct InsetMapView_Previews: PreviewProvider {
+    static var previews: some View {
+        InsetMapView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
