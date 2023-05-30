8c6cfd5765c252fa8a871971deea393fa208faab
SevioCorrea <102820124+SevioCorrea@users.noreply.github.com>
2023-05-30 11:27:21 -0300

    DetailView

---
+++ AnimalsSwiftUI/View/AnimalDetailView.swift
@@ -0,0 +1,76 @@
+//
+//  AnimalDetailView.swift
+//  AnimalsSwiftUI
+//
+//  Created by Sévio Basilio Corrêa on 30/05/23.
+//
+
+import SwiftUI
+
+struct AnimalDetailView: View {
+    
+    // MARK: - Propriedades
+    let animal: Animal
+    
+    // MARK: - Body
+    var body: some View {
+        ScrollView(.vertical, showsIndicators: false) {
+            VStack(alignment: .center, spacing: 20) {
+                // Hero Image
+                Image(animal.image)
+                    .resizable()
+                    .scaledToFit()
+                
+                // Title
+                Text(animal.name.uppercased())
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .multilineTextAlignment(.center)
+                    .padding(.vertical, 8)
+                    .foregroundColor(.primary)
+                    .background(
+                        Color.accentColor
+                            .frame(height: 6)
+                            .offset(y: 24)
+                    )
+                
+                // Headline
+                Text(animal.headline)
+                    .font(.headline)
+                    .multilineTextAlignment(.leading)
+                    .foregroundColor(.accentColor)
+                    .padding(.horizontal)
+                
+                // Gallery
+                Group {
+                    HeadingView(headingImage: "photo.on.rectangle.angled", headingText: "Wilderness in Pictures")
+                    
+                    InsetGalleryView(animal: animal)
+                }
+                .padding(.horizontal)
+                
+                // Facts
+                
+                //Description
+                
+                // Map
+                
+                // Link
+                
+                
+            } //: VStack
+            .navigationBarTitle("Learn about \(animal.name)", displayMode: .inline)
+        } //: Scroll
+    }
+}
+
+// MARK: - Preview
+
+struct AnimalDetailView_Previews: PreviewProvider {
+    static let animals: [Animal] = Bundle.main.decode("animals.json")
+    static var previews: some View {
+        NavigationView {
+            AnimalDetailView(animal: animals[0])
+        }
+    }
+}
---
+++ AnimalsSwiftUI/View/HeadingView.swift
@@ -0,0 +1,39 @@
+//
+//  HeadingView.swift
+//  AnimalsSwiftUI
+//
+//  Created by Sévio Basilio Corrêa on 30/05/23.
+//
+
+import SwiftUI
+
+struct HeadingView: View {
+    
+    // MARK: - Propriedades
+    var headingImage: String
+    var headingText: String
+    
+    // MARK: - Body
+    var body: some View {
+        HStack {
+            Image(systemName: headingImage)
+                .foregroundColor(.accentColor)
+                .imageScale(.large)
+            
+            Text(headingText)
+                .font(.title3)
+                .fontWeight(.bold)
+        }
+        .padding(.vertical)
+    }
+}
+
+// MARK: - Preview
+
+struct HeadingView_Previews: PreviewProvider {
+    static var previews: some View {
+        HeadingView(headingImage: "photo.on.rectangle.angled", headingText: "Wilderness in Pictures")
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
