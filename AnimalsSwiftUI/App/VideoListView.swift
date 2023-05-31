969a37d25b6c673853e093690d181b0ac4d2c64d
SevioCorrea <102820124+SevioCorrea@users.noreply.github.com>
2023-05-31 13:52:35 -0300

    VideoListView

--- AnimalsSwiftUI/App/VideoListView.swift
+++ AnimalsSwiftUI/App/VideoListView.swift
@@ -8,10 +8,40 @@
 import SwiftUI
 
 struct VideoListView: View {
+    
+    // MARK: - Propriedades
+    @State var videos: [Video] = Bundle.main.decode("videos.json")
+    let hapticImpact = UIImpactFeedbackGenerator(style: .medium)
+    
+    // MARK: - Body
+    
     var body: some View {
-        Text("Video")
+        NavigationView {
+            List {
+                ForEach(videos) { item in
+                    VideoListItemView(video: item)
+                        .padding(.vertical, 8)
+                } //: Loop
+            } //: List
+            .listStyle(InsetGroupedListStyle())
+            .navigationBarTitle("Videos", displayMode: .inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: {
+                    // Shuffle videos
+                        videos.shuffle()
+                        hapticImpact.impactOccurred()
+                    }) {
+                        Image(systemName: "arrow.2.squarepath")
+                    }
+
+                }
+            }
+        } //: Navigation
     }
 }
+
+// MARK: - Preview
 
 struct VideoListView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ AnimalsSwiftUI/View/VideoListItemView.swift
@@ -0,0 +1,57 @@
+//
+//  VideoListItemView.swift
+//  AnimalsSwiftUI
+//
+//  Created by Sévio Basilio Corrêa on 31/05/23.
+//
+
+import SwiftUI
+
+struct VideoListItemView: View {
+
+	// MARK: - Propriedades
+    let video: Video
+    
+    // MARK: - Body
+    var body: some View {
+        HStack(spacing: 10) {
+            ZStack {
+                Image(video.thumbnail)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 80)
+                .cornerRadius(9)
+                
+                Image(systemName: "play.circle")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 32)
+                    .shadow(radius: 5)
+            } //: ZStack
+            
+            VStack(alignment: .leading, spacing: 10) {
+                Text(video.name)
+                    .font(.title2)
+                    .fontWeight(.heavy)
+                    .foregroundColor(.accentColor)
+                
+                Text(video.headline)
+                    .font(.footnote)
+                    .multilineTextAlignment(.leading)
+                    .lineLimit(2)
+            } //: VStack
+            
+        } //: HStack
+    }
+}
+
+// MARK: - Preview
+
+struct VideoListItem_Previews: PreviewProvider {
+    static let videos: [Video] = Bundle.main.decode("videos.json")
+    static var previews: some View {
+        VideoListItemView(video: videos[0])
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
